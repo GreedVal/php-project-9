37938a6ec00cf4f63d49a72372166b2019ea792c
GreedVal <137034289+GreedVal@users.noreply.github.com>
2025-02-16 18:35:42 +0300

    greedval

---
+++ routes/app.php
@@ -0,0 +1,8 @@
+<?php 
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UrlsController;
+
+$app->get('/', [HomeController::class, 'index'])->setName('home');
+
+$app->get('/urls', [UrlsController::class, 'index'])->setName('urls');
